b238e0786679a359fa540c1aa30dfae2fa547c8a
aregMeliksetyan <aregm83@gmail.com>
2024-06-28 22:14:46 -0230

    Update script.js
    Code to use new words

--- script.js
+++ script.js
@@ -1,20 +1,68 @@
+
 const words = [
+    { text: "աղբաման", audio: new Audio("audio/Աղբաման.m4a") },
+    { text: "ավտոբուս", audio: new Audio("audio/Ավտոբուս.m4a") },
+    
+    { text: "բաժակ", audio: new Audio("audio/Bajak.m4a") },
+    { text: "բասկետբոլ", audio: new Audio("audio/Բասկետբոլ.m4a") },
     { text: "բարև", audio: new Audio("audio/barev.m4a") },
-    { text: "մանկապարտեզ", audio: new Audio("audio/mankapartez.m4a") },
-    { text: "կարմիր", audio: new Audio("audio/karmir.m4a") },
-    { text: "զրո", audio: new Audio("audio/0.m4a") },
-    { text: "քվադրիլիոն", audio: new Audio("audio/10to15.m4a") },
-    { text: "մեկ", audio: new Audio("audio/1.m4a") },
+    
+    { text: "դպրոց", audio: new Audio("audio/Դպրոց_1.m4a") },
+    
     { text: "երկու", audio: new Audio("audio/2.m4a") },
-    { text: "սեղան", audio: new Audio("audio/Seghan.m4a") },
-    { text: "ճանճ", audio: new Audio("audio/tshantsh.m4a") },
-//    { text: "տատիկ", audio: new Audio("audio/Tatik.m4a") },
-    { text: "սուպեր", audio: new Audio("audio/Super.m4a") },
-    { text: "մարիո", audio: new Audio("audio/Mario.m4a") },
-    { text: "ութ", audio: new Audio("audio/8.m4a") },
-    { text: "քսան", audio: new Audio("audio/20.m4a") },
-    { text: "միլիոն", audio: new Audio("audio/1000000.m4a") },
+    
+    { text: "զրո", audio: new Audio("audio/0.m4a") },
+    
+    { text: "ժամացույց", audio: new Audio("audio/Ժամացույց_1.m4a") },
+    
+    { text: "իննսուն", audio: new Audio("audio/90.m4a") },
+    
+    { text: "լողավազան", audio: new Audio("audio/Loghavazan.m4a") },
+    
+    { text: "խնդրում եմ", audio: new Audio("audio/KhndrumEm.m4a") },
+    
+    { text: "կավիճ", audio: new Audio("audio/Kavich.m4a") },
+    { text: "կարմիր", audio: new Audio("audio/karmir.m4a") },
+    
+    { text: "հազար", audio: new Audio("audio/1000.m4a") },
     { text: "համակարգիչ", audio: new Audio("audio/Hamakargich.m4a") },
+    { text: "հայրիկ", audio: new Audio("audio/Hayrik.m4a") },
+    { text: "հեռախոս", audio: new Audio("audio/Herakhos.m4a") },
+    
+    { text: "ճանճ", audio: new Audio("audio/tshantsh.m4a") },
+    
+    { text: "մայրիկ", audio: new Audio("audio/Maytik.m4a") },
+    { text: "մանկապարտեզ", audio: new Audio("audio/mankapartez.m4a") },
+    { text: "մարիո", audio: new Audio("audio/Mario.m4a") },
+    { text: "մարկեր", audio: new Audio("audio/marker.m4a") },
+    { text: "մեկ", audio: new Audio("audio/1.m4a") },
+    { text: "միլիոն", audio: new Audio("audio/1000000.m4a") },
+    
+    { text: "յոթանասուն", audio: new Audio("audio/70.m4a") },
+    
+    { text: "շնորհակալություն", audio: new Audio("audio/ThankYou.m4a") },
+    
+    { text: "ութ", audio: new Audio("audio/8.m4a") },
+    { text: "ութսուն", audio: new Audio("audio/80.m4a") },
+    
+    { text: "պատուհան", audio: new Audio("audio/Patuhan.m4a") },
+    { text: "պատրաստել", audio: new Audio("audio/Patrastel.m4a") },
+    
+    { text: "սեղան", audio: new Audio("audio/Seghan.m4a") },
+    { text: "սկյուռիկ", audio: new Audio("audio/Սկյուռիկ_1.m4a") },
+    { text: "սուպեր", audio: new Audio("audio/Super.m4a") },
+    
+    { text: "վեց", audio: new Audio("audio/6.m4a") },
+    
+    { text: "տասնհինգ", audio: new Audio("audio/15.m4a") },
+    { text: "տատիկ", audio: new Audio("audio/Tatik.m4a") },
+    
+    { text: "փողոց", audio: new Audio("audio/Փողոց.m4a") },
+    
+    { text: "քսան", audio: new Audio("audio/20.m4a") },
+    { text: "քվադրիլիոն", audio: new Audio("audio/10to15.m4a") },
+    
+    { text: "ֆուտբոլ", audio: new Audio("audio/Ֆուտբոլ_1.m4a") },
 ];
 
 let selectedWords = [];
